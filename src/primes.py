4a834530bfd7145bc810c682dae41788e5ad8d75
Santiago Mout <santiagomout5@gmail.com>
2026-03-23 16:20:29 -0300

    primes.py editado

--- src/primes.py
+++ src/primes.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 # Python program to display all the prime numbers within an interval
 
-lower = 1
-upper = 500
+lower = 1 # Numero desde el cual se buscan primos
+upper = 500 # Numero hasta el que se buscan los primos
 
+print("Archivo Editado.")
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
+   # all prime numbers are greater than lower (1)
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
